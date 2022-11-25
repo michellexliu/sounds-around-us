3d379420f748581dc2306b0e68feece2df514316
Michelle <mliuu3259@gmail.com>
2022-11-24 23:14:56 -0500

    home page styling

--- frontend/src/pages/Login/Login.js
+++ frontend/src/pages/Login/Login.js
@@ -1,10 +1,41 @@
 import React from "react";
+import { TypeAnimation } from "react-type-animation";
+
+import styles from "./Login.module.css";
 
 function Login() {
   return (
-    <a className="btn-spotify" href="/auth/login">
-      Login with Spotify
-    </a>
+    <div className={styles.container}>
+      <h1>The Sounds Around Us</h1>
+      <p className={styles.subtext}>
+        What does&nbsp;
+        <TypeAnimation
+          // Same String at the start will only be typed once, initially
+          sequence={[
+            "love",
+            2000,
+            "heartbreak",
+            2000,
+            "grief",
+            2000,
+            "joy",
+            2000,
+            "optimism",
+            2000,
+            "nostalgia",
+            2000,
+          ]}
+          speed={40} // Custom Speed from 1-99 - Default Speed: 40
+          style={{ fontSize: "2em" }}
+          wrapper="span" // Animation will be rendered as a <span>
+          repeat={Infinity} // Repeat this Animation Sequence infinitely
+        />
+        sound like?
+      </p>
+      <a className="btn" href="/auth/login">
+        Login with Spotify
+      </a>
+    </div>
   );
 }
 
